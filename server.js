8cc1ec18e02b3f0bf3004f9578fdab4af4736f4d
unknown <George>
2019-11-08 14:54:22 +0400

    18 Get All Profiles & Profile By User ID

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('API running'))
+// Init Middleware
+app.use(express.json())
+
+// Connect Database
+connectDB()
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
 
 const PORT = process.env.PORT || 8000
 
